ebe9f9c1534d78fa53eea6360db2ef2d50bf9959
vivek <vivektharhi@gmail.com>
2024-05-05 17:41:45 +0530

    first webflux commit

---
+++ src/test/java/com/vk/webflux/TestWebFluxApplication.java
@@ -0,0 +1,37 @@
+package com.vk.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestWebFluxApplication {
+
+	@Bean
+	@ServiceConnection
+	KafkaContainer kafkaContainer() {
+		return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(WebFluxApplication::main).with(TestWebFluxApplication.class).run(args);
+	}
+
+}
